b373b997998e1df57975f4a359ddff6373c5fb5b
Coffee <coffeethecoder@gmail.com>
2026-07-12 10:20:38 +0530

    postgres linked

---
+++ server/app.js
@@ -0,0 +1,35 @@
+const express = require("express");
+const cors = require("cors");
+const pool = require("./config/db");
+
+require("dotenv").config();
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.get("/", async (req, res) => {
+  try {
+    const result = await pool.query("SELECT NOW()");
+
+    res.status(200).json({
+      success: true,
+      message: "PostgreSQL Connected Successfully",
+      serverTime: result.rows[0].now,
+    });
+  } catch (err) {
+    console.error(err);
+
+    res.status(500).json({
+      success: false,
+      message: "Database Connection Failed",
+    });
+  }
+});
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
