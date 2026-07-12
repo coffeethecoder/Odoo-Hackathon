1d1d61f247247651b7654e9bb4365df9ac6ccbc1
Coffee <coffeethecoder@gmail.com>
2026-07-12 11:57:17 +0530

    Login and signup works now

--- client/pages/auth/login.js
+++ client/pages/auth/login.js
@@ -1,11 +1,15 @@
 const form = document.getElementById("loginForm");
+console.log("login.js loaded");
 
 form.addEventListener("submit", async (e) => {
-
     e.preventDefault();
+
+    console.log("Login button clicked");
 
     const employee_id = document.getElementById("employee_id").value.trim();
     const password = document.getElementById("password").value;
+
+    console.log(employee_id, password);
 
     if (!employee_id || !password) {
         alert("Please fill all fields");
@@ -15,26 +19,25 @@ form.addEventListener("submit", async (e) => {
     try {
 
         const response = await fetch("http://localhost:3001/api/auth/login", {
-
             method: "POST",
-
             headers: {
                 "Content-Type": "application/json",
             },
-
             body: JSON.stringify({
                 employee_id,
                 password
             })
-
         });
 
+        console.log("Status:", response.status);
+
         const data = await response.json();
+
+        console.log(data);
 
         if (data.success) {
 
             alert("Login Successful");
-
             window.location.href = "../dashboard/dashboard.html";
 
         } else {
@@ -46,7 +49,6 @@ form.addEventListener("submit", async (e) => {
     } catch (error) {
 
         console.error(error);
-
         alert("Server Error");
 
     }
